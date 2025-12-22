0e434b050f93f7c940b3a9a4a9ce0ed3995e88eb
ritsuto10969er <rk14oor109@gmail.com>
2025-12-23 04:38:46 +0900

    feat/add fragment

--- react-guide-material/react-guide-material/04_react_basic/src/075_fragment/start/components/Child.jsx
+++ react-guide-material/react-guide-material/04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -1,10 +1,16 @@
 import "./Child.css";
+import React from 'react';
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <React.Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptatibus eos, reprehenderit ipsam magnam voluptatem rerum perspiciatis animi est odio tempore a. Illo dignissimos nisi vero omnis totam voluptatum nobis!</p>
+    </React.Fragment>
+    
   );
 };
 
