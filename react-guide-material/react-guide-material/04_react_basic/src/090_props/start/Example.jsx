3f1b65a3d2fd72d15d88bdd597dce4a9f3495cb5
ritsuto10969er <rk14oor109@gmail.com>
2025-12-23 16:40:52 +0900

    feat/add basic props

--- react-guide-material/react-guide-material/04_react_basic/src/090_props/start/Example.jsx
+++ react-guide-material/react-guide-material/04_react_basic/src/090_props/start/Example.jsx
@@ -1,5 +1,13 @@
 import Child from "./components/Child";
 
-const Example = () => <Child />;
+const Example = () => {
+    return (
+        <>
+            <Child />
+            <Child color = 'red'/>
+            <Child color = 'green'/>
+        </>
+    )
+};
 
 export default Example;
