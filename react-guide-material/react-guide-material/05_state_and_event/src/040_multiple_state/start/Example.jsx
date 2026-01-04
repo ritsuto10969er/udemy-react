5d4bf502dbc7f4b26f61cf664e5a733cad1c8aac
ritsuto10969er <rk14oor109@gmail.com>
2026-01-05 00:53:26 +0900

    feat:add multiple state

--- react-guide-material/react-guide-material/05_state_and_event/src/040_multiple_state/start/Example.jsx
+++ react-guide-material/react-guide-material/05_state_and_event/src/040_multiple_state/start/Example.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+ 
 const Example = () => {
-  
+    const [countA, setCountA]  = useState(0);
+    const [countB, setCountB]  = useState(10);
+   return (
+    <>
+        <p>Button A を{countA}回押しました</p>
+        <button onClick = {() => 
+            setCountA(countA +1)
+        }>Button A</button>
+
+        <p>Button B を{countB}回押しました</p>
+        <button onClick = {() => 
+            setCountB(countB +1 )
+        }>Button B</button>
+    </>
+   )
 };
 
 export default Example;
