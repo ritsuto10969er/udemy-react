ead21dd1ec90d01973020579543ff533a57b09f5
ritsuto10969er <rk14oor109@gmail.com>
2026-01-05 01:17:48 +0900

    feat/add previous state

--- react-guide-material/react-guide-material/05_state_and_event/src/050_prev_state/start/Example.jsx
+++ react-guide-material/react-guide-material/05_state_and_event/src/050_prev_state/start/Example.jsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+  const [count, setCount] = useState(0);
+  const countUp =() => {
+    setCount(count + 1);
+    setCount(prevstate => prevstate + 1);
+  }
+
+  const countDown = () => {
+    setCount(count - 1);
+  }
+  return (
+    <>
+        <p>Current count:{count}</p>
+        <button onClick = {countUp}>+</button>
+
+        <button onClick = {countDown}>-</button>
+    </>
+  )
 };
 
 export default Example;
